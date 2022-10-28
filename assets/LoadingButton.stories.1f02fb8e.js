44372f039eed58cad56b54d9695f0e3a0a56d30c
t-systems-innovation-lab <innolab_tech@t-systems.com>
2022-10-28 09:58:56 +0000

    new files added

---
+++ assets/LoadingButton.stories.1f02fb8e.js
@@ -0,0 +1,64 @@
+var h=Object.defineProperty;var s=(e,a)=>h(e,"name",{value:a,configurable:!0});import{d as O,t as x,a as T,b as c,w as N,u as n,n as k,r as p,o as u,e as I,f as z,g as m}from"./vue.esm-bundler.be6c6897.js";const i=O({__name:"LoadingButton",props:{loading:{type:Boolean},size:{default:"large"},fullWidth:{type:Boolean,default:!0},disabled:{type:Boolean},iconOnly:{type:Boolean},variant:{default:"primary"}},emits:["click"],setup(e,{emit:a}){const g=e,{loading:l,fullWidth:y,size:f,variant:r,disabled:b,iconOnly:d}=x(g),L=T(()=>r.value==="primary"?"white":"primary"),B=s(t=>{t.preventDefault(),t.stopPropagation(),a("click")},"onClick");return(t,W)=>{const _=p("scale-loading-spinner"),v=p("scale-button");return u(),c(v,{variant:n(r),class:k(n(y)?"fullWidth":""),size:n(f),disabled:n(b)||n(l),onClick:B,iconOnly:n(d)},{default:N(()=>[n(l)?m("",!0):I(t.$slots,"default",{key:0},()=>[z("Speichern")]),n(l)?(u(),c(_,{key:1,text:n(d)?"":"Laden...",variant:n(L)},null,8,["text","variant"])):m("",!0)]),_:3},8,["variant","class","size","disabled","iconOnly"])}}});i.__docgenInfo={exportName:"default",displayName:"LoadingButton",description:"",tags:{},props:[{name:"loading",required:!1,type:{name:"boolean"}},{name:"size",required:!1,type:{name:"union",elements:[{name:'"small"'},{name:'"large"'}]},defaultValue:{func:!1,value:"'large'"}},{name:"fullWidth",required:!1,type:{name:"boolean"},defaultValue:{func:!1,value:"true"}},{name:"disabled",required:!1,type:{name:"boolean"}},{name:"iconOnly",required:!1,type:{name:"boolean"}},{name:"variant",required:!1,type:{name:"union",elements:[{name:'"primary"'},{name:'"secondary"'},{name:'"ghost"'}]},defaultValue:{func:!1,value:"'primary'"}}],events:[{name:"click"}],slots:[{name:"default"}]};const M={parameters:{storySource:{source:`import LoadingButton from '../../src/components/LoadingButton.vue';
+//import "@telekom/scale-components/dist/scale-components/scale-components.css";
+
+
+export default {
+  title: 'Example/LoadingButton',
+  component: LoadingButton,
+  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
+  argTypes: {
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'large'],
+    },
+    variant: {
+        control: { type: 'select' },
+        options: ['primary', 'secondary'],
+      },
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
+const Template = (args) => ({
+  // Components used in your story \`template\` are defined in the \`components\` object
+  components: { LoadingButton },
+  // The story's \`args\` need to be mapped into the template through the \`setup()\` method
+  setup() {
+    return { args };
+  },
+  // And then the \`args\` are bound to your component with \`v-bind="args"\`
+  template: '<LoadingButton v-bind="args" />',
+});
+
+
+
+export const Loading = Template.bind({});
+Loading.args = {
+  loading: true,
+};
+
+export const NotLoading = Template.bind({});
+NotLoading.args = {
+    loading: false,
+};
+
+export const IconOnly = Template.bind({});
+IconOnly.args = {
+    loading: true,
+    iconOnly: true
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+    disabled: true,
+};
+export const IconOnlyNotLoading = Template.bind({});
+IconOnlyNotLoading.args = {
+    loading: false,
+    iconOnly: true
+};
+
+
+
+`,locationsMap:{loading:{startLoc:{col:17,line:22},endLoc:{col:2,line:31},startBody:{col:17,line:22},endBody:{col:2,line:31}},"not-loading":{startLoc:{col:17,line:22},endLoc:{col:2,line:31},startBody:{col:17,line:22},endBody:{col:2,line:31}},"icon-only":{startLoc:{col:17,line:22},endLoc:{col:2,line:31},startBody:{col:17,line:22},endBody:{col:2,line:31}},disabled:{startLoc:{col:17,line:22},endLoc:{col:2,line:31},startBody:{col:17,line:22},endBody:{col:2,line:31}},"icon-only-not-loading":{startLoc:{col:17,line:22},endLoc:{col:2,line:31},startBody:{col:17,line:22},endBody:{col:2,line:31}}}}},title:"Example/LoadingButton",component:i,argTypes:{size:{control:{type:"select"},options:["small","large"]},variant:{control:{type:"select"},options:["primary","secondary"]}}},o=s(e=>({components:{LoadingButton:i},setup(){return{args:e}},template:'<LoadingButton v-bind="args" />'}),"Template"),C=o.bind({});C.args={loading:!0};const q=o.bind({});q.args={loading:!1};const V=o.bind({});V.args={loading:!0,iconOnly:!0};const w=o.bind({});w.args={disabled:!0};const D=o.bind({});D.args={loading:!1,iconOnly:!0};const S=["Loading","NotLoading","IconOnly","Disabled","IconOnlyNotLoading"];export{w as Disabled,V as IconOnly,D as IconOnlyNotLoading,C as Loading,q as NotLoading,S as __namedExportsOrder,M as default};
+//# sourceMappingURL=LoadingButton.stories.1f02fb8e.js.map
---
+++ assets/scale-ssr-slot-fix.entry.025baff6.js
@@ -0,0 +1,2 @@
+var i=Object.defineProperty;var t=(r,o)=>i(r,"name",{value:o,configurable:!0});import{r as l,h as s,H as e}from"./preview.2f1330a9.js";import"./index.97c0ef98.js";import"./iframe.755b6b84.js";import"./es.map.constructor.a52a7480.js";import"./decorateStory.7f282716.js";import"./vue.esm-bundler.be6c6897.js";const _=t(class{constructor(r){l(this,r)}render(){return s(e,null,s("slot",null))}},"SsrSlotFix");export{_ as scale_ssr_slot_fix};
+//# sourceMappingURL=scale-ssr-slot-fix.entry.025baff6.js.map
