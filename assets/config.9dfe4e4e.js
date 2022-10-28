e35dc2661269fb1f1bbe47b9b226e9b1f9cb0230
t-systems-innovation-lab <innolab_tech@t-systems.com>
2022-10-28 09:53:29 +0000

    new files added

--- assets/config.9dfe4e4e.js
+++
@@ -1,2 +0,0 @@
-import{d as t,r as m,a as s}from"./decorateStory.0d3f57e8.js";import"./iframe.73313c3a.js";import"./vue.esm-bundler.be6c6897.js";var a={framework:"vue3"};export{t as applyDecorators,a as parameters,m as render,s as renderToDOM};
-//# sourceMappingURL=config.9dfe4e4e.js.map
